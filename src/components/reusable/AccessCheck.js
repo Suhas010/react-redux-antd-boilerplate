77e9f2205b225e2c1108538fef618bb67da83cb4
Suhas More <suhas.more@joshsoftware.com>
2019-04-04 14:27:37 +0530

    Access check

--- src/components/reusable/AccessCheck.js
+++ src/components/reusable/AccessCheck.js
@@ -1,21 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class AccessCheck extends Component {
-  constructor(props){
-    super(props);
-  }
+const AcceccCheck = ({
+  children
+}) => {
   
-  canAccess = () => {
-    return true;
-  }
-
-  render() {
-    if (this.canAccess()) {
-      return this.props.child;
-    }
-    return null;
-  }
 }
-
-export default AccessCheck;
-
