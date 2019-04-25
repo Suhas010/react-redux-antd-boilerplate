d094e91b3e0eee8e87cfca26ec77477c6c9a8425
Suhas More <suhas.more@joshsoftware.com>
2019-04-25 12:59:52 +0530

    removed app config

--- src/actions/appActions/AppConfigActions.js
+++ src/actions/appActions/AppConfigActions.js
@@ -1,53 +1,5 @@
 import RequestHandler from '../../components/helpers/RequestHandler';
 
-export function getConfig() {
-  return RequestHandler.get('/config');
-}
-
-export function getProfiles() {
-  return RequestHandler.profileGet('/config');
-}
-
-export function getCategory(categoryID) {
-  return RequestHandler.get(`/categories/${categoryID}`);
-}
-
-export function getCategories() {
-  return RequestHandler.get('/categories');
-}
-
-export function addCategory(payload) {
-  return RequestHandler.post('/categories', payload);
-}
-
-export function updateCategory(id, payload) {
-  return RequestHandler.put(`/categories/${id}`, payload);
-}
-
-export function deleteCategory(id) {
-  return RequestHandler.delete(`/categories/${id}`);
-}
-
-export function getSubCategory(subCategoryID) {
-  return RequestHandler.get(`/categories/${subCategoryID}`);
-}
-
-export function getSubCategories(id) {
-  return RequestHandler.get(`/categories/${id}/sub_categories`);
-}
-
-export function addSubCategory(categoryID, payload) {
-  return RequestHandler.post(`/categories/${categoryID}/sub_categories`, payload);
-}
-
-export function updateSubCategory(categoryID, subCategoryID, payload) {
-  return RequestHandler.put(`/categories/${categoryID}/sub_categories/${subCategoryID}`, payload);
-}
-
-export function deleteSubCategory(id) {
-  return RequestHandler.delete(`/categories/${id}`);
-}
-
-export function fetchSearchData(targetGroupID) {
-  return RequestHandler.get(`/target_groups/${targetGroupID}/questions`)
+export default function getConfig() {
+  return RequestHandler.get('/login');
 }
