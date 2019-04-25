e1231c780974dc0f4230a792bd9f244d73b26428
Suhas More <suhas.more@joshsoftware.com>
2019-04-25 12:59:12 +0530

    initial commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,38 +1,19 @@
 /* eslint-disable no-undef */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import { Alert, Skeleton } from 'antd';
-import './App.scss';
-import routes from '../../utils/routes';
 import AppLayout from '../layout/AppLayout';
 import DashBoardLayout from '../layout/DashBoardLayout';
 
-const App = () => {
-  const [online, changeStatus] = useState(true);
 
-  useEffect(() => {
-    window.addEventListener('online', () => {
-      changeStatus(navigator.onLine);
-    });
-    window.addEventListener('offline', () => {
-      changeStatus(navigator.onLine);
-    });
-  });
-
-  return (
-    <div className="App">
-      <div className="online">
-        {!online && <Alert message="Seems like you lost internet connection." type="error" showIcon closable />}
+const App = () => (
+  <div className="App">
+    <Router>
+      <div>
+        <Route exact path="/" component={AppLayout} />
+        <Route path="/dashboard" component={DashBoardLayout} />
       </div>
-      <Router>
-        <div>
-          <Route exact path="/" component={() => (<Redirect to={{ pathname: routes.root }} />)} />
-          <Route exact path="/admin" component={AppLayout} />
-          <Route path="/admin/dashboard" component={DashBoardLayout} />
-        </div>
-      </Router>
-    </div>
-  );
-};
+    </Router>
+  </div>
+);
 
 export default App;
