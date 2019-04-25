7468117efd5a8b06110e10fd5cd4cdcd74351351
Suhas More <suhas.more@joshsoftware.com>
2019-04-25 13:07:04 +0530

    categories and interest actions/modal

--- src/models/AppModel/Categories.js
+++
@@ -1,9 +0,0 @@
-import BaseModel from '../BaseModel';
-
-export default class CategoriesModel extends BaseModel {
-  static resource = 'categories';
-
-  constructor(properties) {
-    super(properties);
-  }
-}
--- src/models/AppModel/Interest.js
+++
@@ -1,8 +0,0 @@
-import BaseModel from '../BaseModel';
-
-export default class InterestsModel extends BaseModel {
-  static resource = 'interests';
-  constructor(properties) {
-    super(properties);
-  }
-}
